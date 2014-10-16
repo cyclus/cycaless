c9c4f11f47503f8b49c057fdaf87cf7ce74bd44d
Matthew Gidden <matthew.gidden@gmail.com>
2014-10-16 11:50:52 -0500

    removed other agents

---
+++ src/cycaless.h
@@ -0,0 +1,8 @@
+#ifndef CYCALESS_SRC_CYCALESS_H_
+#define CYCALESS_SRC_CYCALESS_H_
+
+#include "batch_reactor.h"
+#include "batch_reactor_tests.h"
+#include "deploy_inst.h"
+
+#endif  // CYCALESS_SRC_CYCALESS_H_
